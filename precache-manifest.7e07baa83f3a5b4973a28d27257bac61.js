2450f75ac1107f87b4852dd8da35c0f05233555a
Mahdi SE <seyedmahdiseydan78@yahoo.com>
2020-04-05 17:07:39 +0430

    Updates

---
+++ precache-manifest.7e07baa83f3a5b4973a28d27257bac61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db28b39a8c2f9cb5128c350a8c59b361",
+    "url": "/asteroids-game/index.html"
+  },
+  {
+    "revision": "64ac7be91c1a7e675dd4",
+    "url": "/asteroids-game/static/css/main.f0d11dae.chunk.css"
+  },
+  {
+    "revision": "600593ecb3cd3570d2bd",
+    "url": "/asteroids-game/static/js/2.0f82d927.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asteroids-game/static/js/2.0f82d927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ac7be91c1a7e675dd4",
+    "url": "/asteroids-game/static/js/main.bcdb08ea.chunk.js"
+  },
+  {
+    "revision": "3f8f81eb0061a4f144aa",
+    "url": "/asteroids-game/static/js/runtime-main.fda98710.js"
+  }
+]);
